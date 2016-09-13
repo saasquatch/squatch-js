a91f36f0dfd919f136d51b8a7d153e8dc5593367
Logan <logan@saasquat.ch>
2016-09-13 03:59:55 +0000

    LV: Added doc comments, docs generator and linting

--- src/api/OpenApi.js
+++ src/api/OpenApi.js
@@ -1,119 +1,208 @@
-import JSONSchema from 'jsonschema';
+import {validate} from 'jsonschema';
 import schema from './schema.json';
 import 'whatwg-fetch';
 
-var validate = JSONSchema.validate;
 
-class OpenApi {
+/**
+ * 
+ * The OpenApi class is a wrapper around the Open Endpoints of the SaaSquatch REST API.
+ * 
+ * The Open Endpoints in the SaaSquatch REST API are endpoints designed to work 
+ * in client applications like the Mobile SDK and Javascript SDK. 
+ * Authentication relies on a User JWT and some API endpoints are unauthenticated. 
+ * Even though the Open Endpoints are designed for client applications, they can 
+ * still be used in server-to-server cases using API Key authentication.
+ * 
+ */
+export class OpenApi {
 
-    //TODO:
-    // - Authenticate with JWT
-    // - Add comments
+  //TODO:
+  // - Authenticate with JWT
+  // - Add comments
 
-    constructor(config) {
-      this.tenantAlias = config.tenantAlias;
-      this.API_KEY = config.API_KEY;
-      this.domain = "https://app.referralsaasquatch.com";
-    }
+  /**
+   * Initialize a new {@link OpenApi} instance.
+   * 
+   * @param {Object} config Config details
+   * @param {string} config.tenantAlias The tenant to access
+   * @param {string} [config.domain='https://app.referralsaasquatch.com'] The server domain. 
+   *    Useful if you want to use a proxy like {@link https://requestb.in/ RequestBin} or {@link https://runscope.com/ Runscope}.
+   * 
+   * @example <caption>Browser example</caption>
+   * var squatchApi = new squatch.OpenApi({tenantAlias:'test_12b5bo1b25125');
+   * 
+   * @example <caption>Browserify/Webpack example</caption>
+   * var OpenApi = require('squatch-js').OpenApi;
+   * var squatchApi = new OpenApi({tenantAlias:'test_12b5bo1b25125');
+   * 
+   * @example <caption>Babel+Browserify/Webpack example</caption>
+   * import {OpenApi} from 'squatch-js';
+   * let squatchApi = new OpenApi({tenantAlias:'test_12b5bo1b25125');
+   */
+  constructor(config) {
+    this.tenantAlias = config.tenantAlias;
+    this.API_KEY = config.API_KEY;
+    this.domain = "https://app.referralsaasquatch.com";
+  }
 
-    createUser(params) {
-      this.validateInput(params, schema.user);
+  /**
+   * This method creates a user and an account in one call. Because this call creates a user, it requires either a write token or an API key.
+   * This is an Open Endpoint and disabled by default. Contact support to enable the open endpoints.
+   * 
+   * {@link https://docs.referralsaasquatch.com/api/methods/#open_list_referrals List Referrals}
+   * 
+   * @param {Object} params The User/Account
+   * @param {string} params.id the ID of user to be created
+   * @param {string} params.accountId the ID of account to be created
+   * @return {Promise<User>} details of the user create
+   */
+  createUser(params) {
+    this._validateInput(params, schema.user);
 
-      let tenant_alias = encodeURIComponent(this.tenantAlias),
-          account_id = encodeURIComponent(params.accountId),
-          user_id = encodeURIComponent(params.id);
+    let tenant_alias = encodeURIComponent(this.tenantAlias),
+      account_id = encodeURIComponent(params.accountId),
+      user_id = encodeURIComponent(params.id);
 
-      let path = `/api/v1/${tenant_alias}/open/account/${account_id}/user/${user_id}`;
-      let url = this.domain + path;
-      return this.doPost(url, JSON.stringify(params));
-    }
+    let path = `/api/v1/${tenant_alias}/open/account/${account_id}/user/${user_id}`;
+    let url = this.domain + path;
+    return this._doPost(url, JSON.stringify(params));
+  }
 
-    lookUpUser(params) {
-      this.validateInput(params, schema.userLookUp);
+  /**
+   * Looks up a user based upon their id and returns their personal information including sharelinks. This endpoint requires a read token or an API key.
+   * 
+   * This is an Open Endpoint and disabled by default. Contact support to enable the open endpoints.
+   * 
+   * {@link https://docs.referralsaasquatch.com/api/methods/#open_get_user Open API Spec}
+   * 
+   * @param {Object} params The User/Account
+   * @param {string} params.id the ID of user to look up
+   * @param {string} params.accountId the ID of account to look up
+   * @return {Promise<User>} User details
+   */
+  lookUpUser(params) {
+    this._validateInput(params, schema.userLookUp);
 
-      let tenant_alias = encodeURIComponent(this.tenantAlias),
-          account_id = encodeURIComponent(params.accountId),
-          user_id = encodeURIComponent(params.id);
+    let tenant_alias = encodeURIComponent(this.tenantAlias),
+      account_id = encodeURIComponent(params.accountId),
+      user_id = encodeURIComponent(params.id);
 
-      let path = `/api/v1/${tenant_alias}/open/account/${account_id}/user/${user_id}`;
-      let url = this.domain + path;
-      return this.doRequest(url);
-    }
+    let path = `/api/v1/${tenant_alias}/open/account/${account_id}/user/${user_id}`;
+    let url = this.domain + path;
+    return this._doRequest(url);
+  }
 
-    getUserByReferralCode(params) {
-      this.validateInput(params, schema.userReferralCode);
+  /**
+   * Looks up a user by their Referral Code
+   * 
+   * @param {Object} params stuff
+   * @param {string} params.referralCode the code used to look up a user
+   * @return {Promise} User details
+   */
+  getUserByReferralCode(params) {
+    this._validateInput(params, schema.userReferralCode);
 
-      let tenant_alias = encodeURIComponent(this.tenantAlias),
-          referral_code = encodeURIComponent(params.referralCode);
+    let tenant_alias = encodeURIComponent(this.tenantAlias),
+      referral_code = encodeURIComponent(params.referralCode);
 
-      let path = `/api/v1/${tenant_alias}/open/user?referralCode=${referral_code}`;
-      let url = this.domain + path;
-      return this.doRequest(url);
-    }
+    let path = `/api/v1/${tenant_alias}/open/user?referralCode=${referral_code}`;
+    let url = this.domain + path;
+    return this._doRequest(url);
+  }
 
-    lookUpReferralCode(params) {
-      this.validateInput(params, schema.userReferralCode);
+  /**
+   * Looks up a referral code
+   * 
+   * @param {Object} params stuff
+   * @param {string} params.referralCode the code used to look up a code
+   * @return {Promise} User details
+   */
+  lookUpReferralCode(params) {
+    this._validateInput(params, schema.userReferralCode);
 
-      let tenant_alias = encodeURIComponent(this.tenantAlias),
-          referral_code = encodeURIComponent(params.referralCode);
+    let tenant_alias = encodeURIComponent(this.tenantAlias),
+      referral_code = encodeURIComponent(params.referralCode);
 
-      let path = `/api/v1/${tenant_alias}/open/code/${referral_code}`;
-      let url = this.domain + path;
-      return this.doRequest(url);
-    }
+    let path = `/api/v1/${tenant_alias}/open/code/${referral_code}`;
+    let url = this.domain + path;
+    return this._doRequest(url);
+  }
 
-    applyReferralCode(params) {
-      this.validateInput(params, schema.applyReferralCode);
+  /**
+   * Applies a referral code
+   * 
+   * @param {Object} params stuff
+   * @param {string} params.id the ID of the User that is referred
+   * @param {string} params.accountId the Account ID of the User that is referred
+   * @param {string} params.referralCode the code to apply
+   * @return {Promise} Stuff
+   */
+  applyReferralCode(params) {
+    this._validateInput(params, schema.applyReferralCode);
 
-      let tenant_alias = encodeURIComponent(this.tenantAlias),
-          referral_code = encodeURIComponent(params.referralCode),
-          account_id = encodeURIComponent(params.accountId),
-          user_id = encodeURIComponent(params.id);
+    let tenant_alias = encodeURIComponent(this.tenantAlias),
+      referral_code = encodeURIComponent(params.referralCode),
+      account_id = encodeURIComponent(params.accountId),
+      user_id = encodeURIComponent(params.id);
 
-      let path = `/api/v1/${tenant_alias}/open/code/${referral_code}/account/${account_id}/user/${user_id}`;
-      let url = this.domain + path;
-      return this.doPost(url, JSON.stringify(""));
-    }
+    let path = `/api/v1/${tenant_alias}/open/code/${referral_code}/account/${account_id}/user/${user_id}`;
+    let url = this.domain + path;
+    return this._doPost(url, JSON.stringify(""));
+  }
 
-    listReferrals() {
-      let tenant_alias = encodeURIComponent(this.tenantAlias);
+  /**
+   * Lists referrals
+   *
+   * @return {Promise} Stuff
+   */
+  listReferrals() {
+    let tenant_alias = encodeURIComponent(this.tenantAlias);
 
-      let path = `/api/v1/${tenant_alias}/open/referrals`;
-      let url = this.domain + path;
-      return this.doRequest(url);
-    }
+    let path = `/api/v1/${tenant_alias}/open/referrals`;
+    let url = this.domain + path;
+    return this._doRequest(url);
+  }
 
-    validateInput (params, schema) {
-      let valid = validate(params, schema);
-      if(!valid.valid) throw valid.errors;
-    }
+  /**
+   * @private
+   */
+  _validateInput(params, schema) {
+    let valid = validate(params, schema);
+    if (!valid.valid) throw valid.errors;
+  }
 
-    doRequest(url) {
-      return fetch(url, {
-        method: 'GET',
-        headers: {
-          'Authorization': 'Basic ' + btoa(":" + this.API_KEY),
-          'Accept': 'application/json',
-          'Content-Type': 'application/json'
-        }
-      }).then(function(response) {
-        return response.json();
-      });
-    }
+  /**
+   * @private
+   */
+  _doRequest(url) {
+    return fetch(url, {
+      method: 'GET',
+      headers: {
+        'Authorization': 'Basic ' + btoa(":" + this.API_KEY),
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      }
+    }).then(function(response) {
+      return response.json();
+    });
+  }
 
-    doPost(url, data) {
-      return fetch(url, {
-        method: 'POST',
-        headers: {
-          'Authorization': 'Basic ' + btoa(":" + this.API_KEY),
-          'Accept': 'application/json',
-          'Content-Type': 'application/json'
-        },
-        body: data
-      }).then(function(response) {
-        return response.json();
-      });
-    }
+  /**
+   * @private
+   */
+  _doPost(url, data) {
+    return fetch(url, {
+      method: 'POST',
+      headers: {
+        'Authorization': 'Basic ' + btoa(":" + this.API_KEY),
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: data
+    }).then(function(response) {
+      return response.json();
+    });
+  }
 
 }
 
--- src/squatch.js
+++ src/squatch.js
@@ -1,18 +1,44 @@
-import OpenApi from './api/OpenApi.js';
+/**
+ * Squatch.js is the Referral SaaSquatch javascript SDK and a one-stop shop to integrate a referral program into your website or web app. 
+ * It can show referral widgets on any website, track users, generate unique referral short links and referral codes, and more.
+ * 
+ * @module squatch
+ */
+import {OpenApi} from './api/OpenApi.js';
 import cookie from './tracking/Cookie.js';
 
-class Squatch {
-  constructor() {
-    this.loaded = 1;
-  }
+export {OpenApi} from './api/OpenApi';
+export {default as cookie} from './tracking/Cookie.js';
 
-  push() {
-    for ( var i = 0; i < arguments.length; i++) {
-      console.log(arguments[i]);
-    }
-
-    return Array.prototype.push.apply(this, arguments);
-  }
+/**
+ * Initializes a static `squatch` global. This sets up:
+ * 
+ *  - `api` a static instance of the {@link OpenApi}
+ * 
+ * @param {Object} config Configuration details
+ * @param {string} config.tenantAlias The tenant alias connects to your account. Note: There are both *live* and *test* tenant aliases.
+ * @returns {void}
+ * @example 
+ * squatch.init({tenantAlias:'test_basbtabstq51v'});
+ */
+export function init(config) {
+    api = new OpenApi({
+        tenantAlias: config.tenantAlias
+    });
 }
 
-export default Squatch;
+/**
+ * Static instance of the {@link OpenApi}. Make sure you call {@link #init init} first
+ *
+ * @type {OpenApi}
+ * @example
+ * squatch.init({tenantAlias:'test_basbtabstq51v'});
+ * squatch.api.createUser({id:'123', accountId:'abc', firstName:'Tom'});
+ */
+export var api = null;
+
+
+export function autofill() {
+    let opts = {};
+    return cookie('name', 'value', opts);
+}
