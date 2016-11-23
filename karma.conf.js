53d31c4480bdad37d036a27c7dd2e606c99a6bec
Logan <logan@saasquat.ch>
2016-11-23 20:22:47 +0000

    LV: Updated testing and linting work with PhantomJS

--- karma.conf.js
+++ karma.conf.js
@@ -3,8 +3,29 @@ module.exports = function(config) {
     // ... normal karma configuration
     frameworks: ['mocha'],
 
-    browsers : ['Chrome', 'Firefox'],
+    browsers: ['PhantomJS_custom'],
 
+    // you can define custom flags
+    customLaunchers: {
+      'PhantomJS_custom': {
+        base: 'PhantomJS',
+        options: {
+          windowName: 'my-window',
+          settings: {
+            webSecurityEnabled: false
+          },
+        },
+        flags: ['--load-images=true'],
+        debug: true
+      }
+    },
+
+    phantomjsLauncher: {
+      // Have phantomjs exit if a ResourceError is encountered (useful if karma exits without killing phantom)
+      exitOnResourceError: true
+    },
+    
+    
     files: [
       // all files ending in "_test"
       {pattern: 'test/*.js', watched: false}
