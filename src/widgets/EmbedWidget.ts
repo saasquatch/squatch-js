8d996362c9ad24b070d0ea3350ce9c0b80c612ef
Jorge Conde <jorge@referralsaasquatch.com>
2019-04-10 16:01:38 -0700

    Update src/widgets/EmbedWidget.ts
    Co-Authored-By: cbot83 <35348171+cbot83@users.noreply.github.com>

--- src/widgets/EmbedWidget.ts
+++ src/widgets/EmbedWidget.ts
@@ -133,7 +133,7 @@ export default class EmbedWidget extends Widget {
           showStatsBtn.id = 'show-stats-btn';
           showStatsBtn.textContent = (this.type === 'REFERRER_WIDGET') ? 'Show Stats' : 'Show Reward';
 
-          showStatsBtn.setAttribute('style', 'margin-top: 10px;');
+          showStatsBtn.setAttribute('style', 'margin-top: 10px; max-width: 130px; width: 100%;');
           showStatsBtn.onclick = () => {
             this.load();
           };
